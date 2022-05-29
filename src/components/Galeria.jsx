75ad3518d9c76e6b7e66a4b39d8c12470b111318
Peter <peterthomaskim@gmail.com>
2022-05-29 16:51:50 -0300

    added subtitles

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -23,15 +23,21 @@ export default function Galeria() {
 
         let randomnumber =
             Math.floor(Math.random() * (maximum - minimum)) + minimum;
-            return String(listwidth[randomnumber]);
-        };
+        return String(listwidth[randomnumber]);
+    };
     // end
 
     return (
         <section className="container mx-auto ">
-            <h2 className="font-bold text-3xl text-center pt-8 pb-12">
-                Lançamentos
+            <h2 className="text-center text-4xl font-bold pt-8">
+                Moda Feminina Atacado Bom Retiro
             </h2>
+            <p className="text-center text-2xl pt-2 pb-6">
+                Criamos, Produzimos e Vendemos em Atacado
+            </p>
+            {/* <h2 className="font-bold text-3xl text-center pt-8 pb-12">
+                Lançamentos
+            </h2> */}
             <div className="grid grid-flow-row-dense grid-cols-12 place-items-center md:grid hidden">
                 {images.map((image, index) => (
                     <img
